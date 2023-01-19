4966500d0d39a4a94221eb66fb8c6e9c798c6e1f
arnindkrn <32215063+arnindkrn@users.noreply.github.com>
2023-01-19 11:11:25 +0700

    Update arnold.m

--- arnold.m
+++ arnold.m
@@ -1,16 +1,24 @@
 function wm = arnold(w,m,key)
-
 wm = w;
-p = 8; q = 10;
-I = [1 p; q p*q+1];
-
 for k = 1:key
     for x = 1:m
         for y = 1:m
-             result = mod (I*[x; y], m);
-             wm(x,y)= w(result(1)+1, result(2)+1);
+             x1 = x + y;
+             y1 = x + 2*y;
+             if x1 > m
+                 x1 = mod(x1,m);
+             end
+             if y1 > m
+                 y1 = mod(y1,m);
+             end
+             if x1 == 0
+                 x1 = m;
+             end
+             if y1 == 0
+                 y1 = m;
+             end
+             wm(x1,y1)= w(x,y);
         end
     end
      w = wm;
-end
-    
+end;
